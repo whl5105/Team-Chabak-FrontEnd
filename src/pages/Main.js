0d14814a8b8b53e4e39b01c5d528632579f45b52
JAEWOO PARK <wodn1199@naver.com>
2021-12-07 12:57:39 +0900

    detail, main view

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Grid } from "../elements";
+
+import Post from "../components/Post";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Grid>
+        <Post></Post>
+      </Grid>
+    </React.Fragment>
+  );
 };
 
 export default Main;
