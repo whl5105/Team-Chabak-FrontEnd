b684c9408ec709cb07c17157b3ba30c8cb0db552
chungune <ja246@naver.com>
2021-12-07 13:06:11 +0900

    add PostWrite

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import React from "react";
+import PostWrite from "../pages/PostWrite";
+
+import { Grid, Button } from "../elements";
+
+import { Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">test</div>;
+
+
+  return (
+    <React.Fragment>
+      <Grid>
+        <Route path='/write' exact component={PostWrite}></Route>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
 export default App;
